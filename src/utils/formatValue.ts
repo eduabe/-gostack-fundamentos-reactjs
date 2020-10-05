847fa6c3ba6349e49cab780a760a9a08a046de4e
Eduardo <eduabe.n@gmail.com>
2020-10-04 22:23:02 -0300

    Finalizado o desafio

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,4 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value);
+
 
 export default formatValue;
